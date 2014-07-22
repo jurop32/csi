98631196a56eed533384207f8d833dae883f06ce
Juro P <jurop44@gmail.com>
2014-07-22 06:02:06 +0200

    total cleanup 1

---
+++ exetra/slideshow.js
@@ -0,0 +1,179 @@
+/**
+ * req. jquery, ui easing effects
+ * IN
+ *     $('#yourelement').slideshow({
+ *         transition: '',
+ *         opts.easing: ''
+ *     });
+ */
+(function($)
+{
+    var the_switch = direction = timer = main = box = slides = null;
+    var cnt = act = 0;
+    var opts = {
+        allowscroll: true,
+        use_switch: true,
+        continuous: true,
+        easing: 'linear',
+        delay: 800,
+        transitionspeed: 800
+    };
+    function slideshow(options)
+    {
+        var args = arguments[0] || {};
+        $.extend(opts, options);
+        main = $(this);
+        box = main.children('ul');
+        slides = box.children('li');
+        cnt = slides.length;
+        if (cnt)
+        {
+            if (opts.use_switch && cnt > 1)
+            {
+                the_switch = $('<ol id="controls" class="list-item-4"></ol>');
+                the_switch.appendTo(main);
+            }
+            slides.css({
+                'display': 'none',
+                'position': 'static'
+            });
+            slides.first().css('display', 'list-item');
+            act = 1;
+            main.hover(stopScroll, allowScroll);
+            slides.each(function(i)
+            {
+                /*if (i == 0)
+                {
+                    slides.parent().append(
+                        slides.first().clone().css({
+                            'position': 'static',
+                            'display': 'block'
+                        })
+                    );
+                }*/
+                $(this).addClass('sl' + ++i);
+                if (opts.use_switch && cnt > 1)
+                {
+                    the_switch.append('<li id="controls' + i + '">' + '<a href="#">' + $(this).attr('title') + '</a></li>');
+                    $('#controls' + i + ' a').on(
+                        'click',
+                        function()
+                        {
+                            scrollToSlide(i);
+                        }
+                    );
+                }
+            });
+            if (opts.use_switch && cnt > 1)
+            {
+                the_switch.append('<li style="clear:both"></li>');
+                the_switch.children('#controls1').addClass('current');
+            }
+            direction = 'left';
+            if (cnt > 1)
+            {
+                timer = setTimeout(scrollSlides, opts.delay * 5);
+            }
+        }
+        return this;
+    };
+    function scrollSlides()
+    {
+        clearTimeout(timer);
+        if (opts.continuous)
+        {
+            // var out = slides.eq(act);
+            var act2 = act;
+            act = (act % cnt) + 1;
+            slides.eq(act-1).css('position', 'static').fadeIn(opts.transitionspeed / 2, opts.easing, function()
+            {
+                setSlideSwitch();
+                timer = setTimeout(scrollSlides, opts.delay * 4);
+            });
+            slides.eq(act2-1).css('position', 'absolute').fadeOut(opts.transitionspeed / 2, opts.easing);
+        } else
+        {
+            if (act >= cnt)
+            {
+                direction = '>';
+            }
+            if (act <= 1)
+            {
+                direction = '<';
+            }
+            if (opts.allowscroll)
+            {
+                if (direction == '<')
+                {
+                    slides.eq(act-1)
+                    // box.children('.sl' + act++)
+                    .fadeOut(opts.transitionspeed / 2, opts.easing);
+                    slides.eq(act++)
+                    //box.children('.sl' + act)
+                    .fadeIn(opts.transitionspeed / 2, opts.easing, function()
+                    {
+                        setSlideSwitch();
+                        timer = setTimeout(scrollSlides, opts.delay * 4);
+                    });
+                } else
+                {
+                    slides.eq(act-1).fadeOut(opts.transitionspeed / 2, opts.easing);
+                    slides.eq(act--).fadeIn(opts.transitionspeed / 2, opts.easing, function()
+                    {
+                        setSlideSwitch();
+                        timer = setTimeout(scrollSlides, opts.delay * 4);
+                    });
+                }
+            }
+        }
+    }
+    function stopScroll()
+    {
+        if (cnt != 1)
+        {
+            opts.allowscroll = false;
+            clearTimeout(timer);
+        }
+    }
+    function allowScroll()
+    {
+        if (cnt != 1)
+        {
+            opts.allowscroll = true;
+            timer = setTimeout(scrollSlides, opts.delay);
+        }
+    }
+    function scrollToSlide(i)
+    {
+        slides.eq(i-1).css('position', 'static').fadeIn(opts.transitionspeed / 2, opts.easing, function()
+        {
+            setSlideSwitch();
+        });
+        slides.eq(act - 1).stop().css('position', 'absolute').fadeOut(opts.transitionspeed / 2, opts.easing);
+        act = i;
+    }
+    function setSlideSwitch()
+    {
+        if (opts.use_switch)
+        {
+            the_switch.children('.current').removeClass('current');
+            the_switch.children('#controls' + act).addClass('current');
+        }
+    }
+    function nextSlide()
+    {
+        if (cnt > act)
+        {
+            scrollToSlide(act + 1);
+        }
+    }
+    function prevSlide()
+    {
+        if (act > 1)
+        {
+            scrollToSlide(act - 1);
+        }
+    }
+    $.fn.slideshow = slideshow;
+    $.fn.scrollToSlide = scrollToSlide;
+})(jQuery);
---
+++ templates/scripts/global.js
@@ -0,0 +1,179 @@
+$(document).ready(function()
+{
+    // MENU
+    $('.menu li').on('mouseenter mouseleave', function(e)
+    {
+        var menuitem = $(this);
+        if (e.type == 'mouseenter')
+        {
+            menuitem.addClass('act');
+        } else
+        {
+            menuitem.removeClass('act');
+        }
+    });
+    // HOME SLIDER
+    if ($('.home-slider').length)
+    {
+        $(".home-slider-in").slideshow();
+    }
+    // FILTER
+    $('.filter.button, .filter-box a.close').on('click', function(e)
+    {
+        $('.filter-box').toggle(0);
+        e.preventDefault();
+    });
+    // PREVIEW IMAGE
+    $('a.preview').on('mouseenter', function(e)
+    {
+        var preview = $(this), alt = preview.parent().find('img').attr('alt');
+        desc = preview.parent().find('p').html(), pictograms = preview.parent().find('.pict').html();
+        pictograms = pictograms.replace('nopreview', 'yespreview');
+        pictograms = pictograms.replace('rel="ADD TO CART"', 'title="ADD TO CART"');
+        pictograms = pictograms.replace('rel="CALCULATE PRICE"', 'title="CALCULATE PRICE"');
+        pictograms = pictograms.replace('rel="ADD TO LIGHTBOX"', 'title="ADD TO LIGHTBOX"');
+        pictograms = pictograms.replace('rel="LARGE PREVIEW"', 'title="LARGE PREVIEW"');
+        pictograms = pictograms.replace('rel="REMOVE IMAGE"', 'title="REMOVE IMAGE"');
+        pictograms = pictograms.replace('rel="EDIT FRIENDLY IMAGE"', 'title="EDIT FRIENDLY IMAGE"');
+        id_produkt = preview.parent().find('.id_produkt').html();
+        previewLightbox = $('.image-lightbox');
+
+        var au;
+        au = $('#au').html();
+        au = parseInt(au);
+        if (!isNaN(au))
+        {
+            image = preview.parent().find('img').attr('src').replace('_2', '_4');
+            image = image.replace('d=2', 'd=4');
+        } else
+        {
+            image = preview.parent().find('img').attr('src').replace('_2', '_3');
+            image = image.replace('d=2', 'd=3');
+        }
+        if (preview.hasClass('vertical'))
+        {
+            var leftMin = 100, topMin = 100;
+        } else
+        {
+            var leftMin = 100, topMin = 5;
+        }
+        var leftpos = preview.position().left;
+        if (leftpos < 100)
+        {
+            leftpos = preview.offset().left;
+            pictograms = pictograms.replace('yespreview', 'yespreview-offset');
+        }
+        previewLightbox.html('<a style="cursor:pointer;" onclick=\'window.open("/img-detail/?id_image=' + id_produkt + '","mywindow","menubar=0,resizable=1,scrollbars=1,width=850,height=750").focus();\'>' + '<img src="' + image + '" alt="' + alt + '" title="' + alt + '"></a>' + '<p>' + desc + '</p>' + '<div style="position: relative;">' + '<div class="pict xpict">' + pictograms + '</div>' + '<div class="cs_addtocart" style="position: absolute; top:30px; display:none; width: 152px; text-align:center; background-color: #999999; color:#ffffff; padding:3px;" id="div_addtocart' + id_produkt + '">Added to cart</div>' + '</div>' + '<button onclick=\'window.open("/price-quote/?id_image=' + id_produkt + '","mywindow","menubar=0,resizable=1,scrollbars=1,width=850,height=750").focus();\'>Check license</button>' + '<form id=""></form>' + '<script language="javascript">' + '$(".xpict .item-1").click(function() { $(this).parent().next(".cs_addtocart").slideDown("normal", function() {});});</script>').css(
+        {
+            left : leftpos - leftMin,
+            top : preview.position().top - topMin,
+            display : 'block'
+        });
+        $('.image-lightbox').on('mouseleave', function()
+        {
+            previewLightbox.html('').hide();
+        });
+        e.preventDefault();
+    });
+    // INVOICE DETAIL
+    $('.invoices input').change(function()
+    {
+        if ($(this).attr('checked') == 'checked')
+        {
+            $(this).parent().parent().next().removeClass('hide');
+        }
+        if ($(this).attr('checked') != 'checked')
+        {
+            $(this).parent().parent().next().addClass('hide');
+        }
+    });
+    $(document).on('click', '.pict a.item-5, a.show-lightbox-category, .lightbox-category a.close, .lightbox-category button', function()
+    {
+        var item = $(this).attr('class');
+        var id = $(this).attr('id');
+        if ($(this).attr('type') == 'vertical')
+        {
+            var vertical = 1;
+        }
+        if (item == 'item-5 nopreview')
+        {
+            if ($("#keywords_body").is(':visible') && $("#keywords_wrapper").is(':visible'))
+            {
+                var offset_top = -607;
+            } else if ($("#keywords_body").is(':hidden') && $("#keywords_wrapper").is(':visible'))
+            {
+                var offset_top = -462;
+            } else
+            {
+                var offset_top = -420;
+            }
+            $('.lightbox-category').css(
+            {
+                left : $(this).offset().left - 150,
+                top : $(this).offset().top + offset_top,
+                right : 'auto'
+            });
+            $('#id_item_lightbox').val('' + id + '');
+        }
+        if (item == 'item-5 yespreview')
+        {
+            if ($("#keywords_body").is(':visible') && $("#keywords_wrapper").is(':visible'))
+            {
+                var offset_top = -630;
+            } else if ($("#keywords_body").is(':hidden') && $("#keywords_wrapper").is(':visible'))
+            {
+                var offset_top = -485;
+            } else
+            {
+                var offset_top = -443;
+            }
+            if (vertical == 1)
+            {
+                offset_top = offset_top - 120;
+            }
+            $('.lightbox-category').css(
+            {
+                left : $(this).offset().left - 50,
+                top : $(this).offset().top + offset_top,
+                right : 'auto'
+            });
+
+            $('#id_item_lightbox').val('' + id + '');
+        }
+        if (item == 'item-5 yespreview-offset')
+        {
+            if ($("#keywords_body").is(':visible') && $("#keywords_wrapper").is(':visible'))
+            {
+                var offset_top = -620;
+            } else if ($("#keywords_body").is(':hidden') && $("#keywords_wrapper").is(':visible'))
+            {
+                var offset_top = -470;
+            } else
+            {
+                var offset_top = -440;
+            }
+            $('.lightbox-category').css(
+            {
+                left : $(this).offset().left - 120,
+                top : $(this).offset().top + offset_top,
+                right : 'auto'
+            });
+            $('#id_item_lightbox').val('' + id + '');
+        }
+        $('.lightbox-category').toggle(0);
+        previewLightbox.html('').hide();
+        e.preventDefault();
+    });
+    // PICT TOOLTIP
+    $('.pict a').on('mouseenter mouseleave', function(e)
+    {
+        var tooltip = $(this).attr('rel'), tooltipE = $(this).parent().find('span');
+        if (e.type == 'mouseenter')
+        {
+            tooltipE.html(tooltip);
+        } else
+        {
+            tooltipE.html('');
+        }
+    });
+});
